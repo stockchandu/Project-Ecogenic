df008ffa8c71cefaf83cf41efebbbb9640dc77fd
Rounak <rounak_fw11_027@masai.school>
2021-11-04 01:52:53 +0530

    Created the basic structure of the onboard page

--- Rounak/src/App.js
+++ Rounak/src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
+import { useState,useEffect } from 'react';
+import { Onboarding1 } from './components/Onboarding1';
+import { Onboarding2 } from './components/Onboarding2';
+import { Onboarding3 } from './components/Onboarding3';
 
 
 
 function App() {
+  const [count, setCount] = useState(1)
+
+  useEffect(()=>{
+    const id = setInterval(() => {
+        setCount((prev)=>{
+            if(prev === 3){
+                clearInterval(id)
+                return 3;
+            }
+            return Number(prev) + 1;
+        })
+    }, 2000);
+
+    return ()=>{
+        clearInterval(id)
+    }
+
+},[])
+
   return (
+    count===1 ?
     <div>
-      <img src="../images/Group 1981.png" alt="none"/>
+      <Onboarding1/>
+    </div>
+    :
+    count ===2 ?
+    <div>
+      <Onboarding2/>
+    </div>
+    :
+    <div>
+      <Onboarding3/>
     </div>
   );
 }
--- Rounak/src/components/Onboarding2.jsx
+++ Rounak/src/components/Onboarding2.jsx
@@ -1,7 +1,9 @@
+import "./onboarding2.css"
+
 const Onboarding2 = ()=>{
     return(
         <div>
-            <img src="../images/Group 1981.png" alt="" />
+            <img id="ecogenic" src={require("../images/Group 1981.png").default} alt="none"/>
         </div>
     )
 }
