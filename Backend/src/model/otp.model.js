5cfc7d3246551c96e5bf58120a442eb055f24720
stockchandu <malikchandan926@gmail.com>
2021-11-07 16:15:28 +0530

    few updated on resend OTP crud

--- Backend/src/model/otp.model.js
+++ Backend/src/model/otp.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const otpSchema = new mongoose.Schema({
     otp:{type:String},
     expireAt: {
-        type: Date,
+        type: new Date,
         default: Date.now,
         index: { expires: '10m' },
       },
@@ -11,7 +11,7 @@ const otpSchema = new mongoose.Schema({
     timestamps:true
 })
 
-otpSchema.index({ "expire_at": 1 });
+otpSchema.index({ "expire_at": 1 },{ expireAfterSeconds: 300 });
 const Otp = mongoose.model("otpUserData",otpSchema)
 
 module.exports=Otp
