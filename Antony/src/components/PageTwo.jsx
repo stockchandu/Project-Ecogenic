e1aa4e96e6cf630e6a059e5ccf2175454a2f50d0
Antony Chacko <antony101tony@gmail.com>
2021-11-09 18:16:10 +0530

    page 1 started

--- Antony/src/components/PageTwo.jsx
+++ Antony/src/components/PageTwo.jsx
@@ -1,3 +1,47 @@
+import { BottomButton } from "./styledComponents/BottomButton";
+import { Skip } from "./styledComponents/Skipbutton";
+import styled from "styled-components";
+
+const Maindiv = styled.div`
+  width: 100%;
+`;
+const ButtonWrapDiv = styled.div`
+  margin-top: 107px;
+  display: flex;
+  justify-content: center;
+`;
 export const PageTwo = (props) => {
-  return <>page 2</>;
+  return (
+    <Maindiv>
+      <Skip onclick={props.handelSkip} />
+      {/* <Para>
+        Let’s find informative blogs to <br /> follow based on your interests.
+      </Para>
+
+      <ImgWrapDiv>
+        <ImagesDiv>
+          {" "}
+          <img src={im1} alt="" />
+          <img src={im2} alt="" />
+          <img src={im3} alt="" />
+        </ImagesDiv>
+        <ImagesDiv>
+          {" "}
+          <img src={im4} alt="" />
+          <img src={im5} alt="" />
+          <img src={im6} alt="" />
+        </ImagesDiv>
+        <ImagesDiv>
+          {" "}
+          <img src={im7} alt="" />
+          <img src={im8} alt="" />
+          <img src={im9} alt="" />
+        </ImagesDiv>
+      </ImgWrapDiv> */}
+      <ButtonWrapDiv>
+        {" "}
+        <BottomButton onclick={props.handelPage}> continue</BottomButton>
+      </ButtonWrapDiv>
+    </Maindiv>
+  );
 };
