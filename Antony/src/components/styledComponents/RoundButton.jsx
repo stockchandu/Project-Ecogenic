f52599b6c483bdbedbf2df0ff0254851a78f4430
Antony Chacko <antony101tony@gmail.com>
2021-11-10 13:10:16 +0530

    topic interest updates

--- Antony/src/components/styledComponents/RoundButton.jsx
+++ Antony/src/components/styledComponents/RoundButton.jsx
@@ -3,14 +3,7 @@ import react from "react";
 import styled from "styled-components";
 
 export function RoundButton(props) {
-  // console.log(props.isSelected);
   console.log("props boolean guy consoleeeeeeeeeeee", props.state, props.name);
-  // const [sel, setSel] = useState(false);
-  // props.isSelected && setSel(true);
-
-  const [selected, setSelected] = useState(false);
-  // const ref = useRef(false);
-  // console.log("ref at atart after initializing", ref.current);
 
   const Button = styled.button`
     /* padding: 6px; */
@@ -34,22 +27,13 @@ export function RoundButton(props) {
     /* color: #212121; */
     color: ${props.state ? "white" : " #212121"};
     background-color: ${props.state ? " #149f65" : "#ffffff"};
-    border: ${selected ? "none" : "solid ##6c757d 1px"};
+    border: ${props.state ? "none" : "solid ##6c757d 1px"};
   `;
 
-  //   useEffect(() => {
-  //     //setSelected(ref.current);
-  //    // console.log("effect called...............", ref.current);
-  //   }, []);
   return (
     <Button
       onClick={() => {
         props.handleIsSelected(props.name);
-        // setSelected(true);
-        // console.log(props.children);
-        // ref.current = true;
-        //console.log(ref.current, "ref current...........");
-        //   setSelected(ref.current);
       }}
     >
       {" "}
