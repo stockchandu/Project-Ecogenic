272f87f6f1603e6b9f0ef7f41c82a0b7ae99f328
stockchandu <malikchandan926@gmail.com>
2021-11-11 00:54:56 +0530

    few implimentation of razorpay payment gateway

--- Backend/src/model/otp.model.js
+++ Backend/src/model/otp.model.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 const otpSchema = new mongoose.Schema({
     otp:{type:String},
-    expireAt: {
-        type: new Date,
-        default: Date.now,
-        index: { expires: '10m' },
-      },
+    // expireAt: {
+    //     type: new Date,
+    //     default: Date.now,
+    //     index: { expires: '10m' },
+    //   },
 },{
     versionKey:false,
     timestamps:true
 })
 
-otpSchema.index({ "expire_at": 1 },{ expireAfterSeconds: 300 });
+// otpSchema.index({ "expire_at": 1 },{ expireAfterSeconds: 300 });
 const Otp = mongoose.model("otpUserData",otpSchema)
 
 module.exports=Otp
