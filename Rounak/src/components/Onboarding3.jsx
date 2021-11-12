5d86a09a3acc84c1a79a35cf4cb94f92e13c5b2a
Rounak <rounak_fw11_027@masai.school>
2021-11-12 11:51:07 +0530

    Onboarding page, some more changes remaining

--- Rounak/src/components/Onboarding3.jsx
+++ Rounak/src/components/Onboarding3.jsx
@@ -1,12 +1,15 @@
 import "./onboarding3.css"
 
 
-const Onboarding3 = ()=>{
+const Onboarding3 = ({c})=>{
+
+    
+
     return(
         <div>
             <img id="ecogen" src={require("../images/Group 1981.png").default} alt="none"/>
             <img id="getStarted" src={require("../images/Component 2.png").default} alt="none"/>
-            <p id="p1">Already have an account? Login</p>
+            <p id="p1" onClick={c}>Already have an account? Login</p>
             <p id="p2">Terms of service/ Privacy Policy</p>
         </div>
     )
