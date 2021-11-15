c502be7f2ce78cffbb57df025feb30195e2aa90c
Antony Chacko <antony101tony@gmail.com>
2021-11-15 21:56:02 +0530

    few updates

--- Frontend/src/components/PriceStrategy/StrategySlide.jsx
+++ Frontend/src/components/PriceStrategy/StrategySlide.jsx
@@ -295,11 +295,3 @@ export function StrategySlider(props) {
     </>
   );
 }
-
-// Don't forget to include the css in your page
-
-// Using webpack or parcel with a style loader
-// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-// Using html tag:
-// <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
